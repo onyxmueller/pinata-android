a7e2e2852ab717968ada65ee22e785155112169d
Onyx Mueller <onyx@onyxmueller.net>
2024-12-14 21:08:14 -0700

    feat: minify release (#8)

--- pinata/build.gradle.kts
+++ pinata/build.gradle.kts
@@ -56,7 +56,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
