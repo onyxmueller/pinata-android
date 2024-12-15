003bfe643d932a916eb2039f79adcf4e03a2a2c5
Onyx Mueller <onyx@onyxmueller.net>
2024-12-14 21:06:00 -0700

    feat: minify release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":pinata"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -66,11 +67,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(project(":pinata"))
-    implementation(libs.retrofit)
 }
