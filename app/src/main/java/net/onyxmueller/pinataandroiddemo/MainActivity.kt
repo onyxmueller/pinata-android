a83b02b1e1aff1fa29173458df6c063e8ca94f98
Onyx Mueller <onyx@onyxmueller.net>
2024-12-15 23:18:21 -0700

    chore: update demo app

--- app/src/main/java/net/onyxmueller/pinataandroiddemo/MainActivity.kt
+++ app/src/main/java/net/onyxmueller/pinataandroiddemo/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.CoroutineScope
@@ -20,8 +20,6 @@ import net.onyxmueller.pinata.onError
 import net.onyxmueller.pinata.onException
 import net.onyxmueller.pinata.onSuccess
 import net.onyxmueller.pinataandroiddemo.ui.theme.PinataAndroidSampleAppTheme
-import java.io.File
-import java.io.FileOutputStream
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,14 +27,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PinataAndroidSampleAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    DemoAppMessage()
                 }
             }
         }
+
+        // Temporary demonstration of the Pinata Android library.
+        // More to come!
         val pinataClient =
             PinataClient.get(
                 BuildConfig.PINATA_JWT_TOKEN,
@@ -44,86 +45,46 @@ class MainActivity : ComponentActivity() {
             )
         CoroutineScope(Dispatchers.IO).launch {
             val pinataApiResult = pinataClient.files.list(order = Order.ASC)
-            var firstFileId = ""
-            var firstFileCid = ""
             pinataApiResult.onSuccess { response ->
-                println(response.files)
-                firstFileId = response.files[0].id
-                firstFileCid = response.files[0].cid
+                setContent {
+                    PinataAndroidSampleAppTheme {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            PinataMessage(response.files.size.toString())
+                        }
+                    }
+                }
             }.onError { code, message ->
                 TODO("Not yet implemented")
             }.onException {
                 TODO("Not yet implemented")
-            }
-
-            val getResponsePinataApiResult = pinataClient.files.get(firstFileId)
-            getResponsePinataApiResult.onSuccess {
-                    getResponse ->
-                println(getResponse.name)
-            }.onError { code, message ->
-                println("Error: $code")
-            }.onException { exception ->
-                println("Exception: $exception")
-            }
-
-            val signResponsePinataApiResult = pinataClient.files.sign(firstFileCid, 604800)
-            signResponsePinataApiResult.onSuccess { signResponse ->
-                println("Sign URL: $signResponse")
-            }.onError { code, message ->
-                println("Error: $code\nMessage: $message")
-            }.onException { exception ->
-                println("Exception: $exception")
-            }
-
-            val filesDir = applicationContext.filesDir
-            val file = File(filesDir, "image.jpg")
-            val outputStream = FileOutputStream(file)
-            val am = applicationContext.assets
-            val inputStream = am.open("waterfall.jpg")
-            inputStream.copyTo(outputStream)
-
-            val uploadPinataApiResult = pinataClient.files.upload(file, "waterfall.jpg")
-            uploadPinataApiResult.onSuccess { uploadResponse ->
-                println("CID: ${uploadResponse.cid}")
-            }.onError { code, message ->
-                println("Error: $code\nMessage: $message")
-            }.onException { exception ->
-                println("Exception: $exception")
-            }
-
-//            val deletePinataApiResult = pinataClient.files.delete(firstFileId)
-//            deletePinataApiResult.onSuccess { deleteResponse ->
-//                println("File deleted")
-//            }.onError { code, message ->
-//                println("Error: $code\nMessage: $message")
-//            }.onException { exception ->
-//                println("Exception: $exception")
-//            }
-
-            val updatePinataApiResult = pinataClient.files.update(firstFileId, "15.jpg", mapOf("location" to "Earth"))
-            updatePinataApiResult.onSuccess { updateResponse ->
-                println("Name: ${updateResponse.name}")
-            }.onError { code, message ->
-                println("Error: $code\nMessage: $message")
-            }.onException { exception ->
-                println("Exception: $exception")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DemoAppMessage(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Demo app of the Pinata Android library.",
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun PinataMessage(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "# of Pinata Files: $message",
         modifier = modifier,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComingSoonMessagePreview() {
     PinataAndroidSampleAppTheme {
-        Greeting("Android")
+        DemoAppMessage()
     }
 }
